61b1c020381a5af70beb7e0d199907170a21e659
balaji-21490 <balaji-21490@balaji-21490>
2024-06-17 09:25:42 +0530

    New Commit

--- src/main/java/com/banking/utils/DatabaseConnection.java
+++ src/main/java/com/banking/utils/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 	private static final String url = "jdbc:mysql://localhost:3306/Banking_Application";
 	private static final String userName = "root";
-	private static final String password = "";
+	private static final String password = "root";
 
 	public static Connection getConnection() throws CustomException {
 		Connection connection = null;
